d804195332039ea53bbbf4e8e135f7a3b387ae13
AashishSec ఆశిష్ <65489287+aashishsec@users.noreply.github.com>
2024-01-18 16:41:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,23 @@
+from setuptools import setup, find_packages
 
+setup(
+    name='httpAlive',
+    version='0.1.0',
+    description='HttpAlive is a web probing tool designed for discovering alive subdomains and URLs.',
+    author='Bande Aashish',
+    url='https://github.com/aashishsec/httpAlive',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'httpx',
+        'colorama',
+    ],
+    extras_require={
+        'dev': ['argparse', 'concurrent.futures'],
+    },
+    entry_points={
+        'console_scripts': [
+            'httpAlive = httpAlive:main',
+        ],
+    },
+)
